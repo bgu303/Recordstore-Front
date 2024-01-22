525c10ee9c8e8fed924fbcd2c1a2ab025035d0b6
bgu303 <bgu303@myy.haaga-helia.fi>
2024-01-22 13:08:20 +0200

    API connection to my backend

--- src/components/Records.js
+++ src/components/Records.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css";
 import 'ag-grid-community/styles/ag-theme-material.css';
@@ -7,31 +7,50 @@ import { Button } from '@mui/material';
 
 function Records() {
 
-    const [records, setRecords] = useState([
-        { artist: "Elvis", title: "Elvis Greatest Hits", label: "Elvis Music Label", size: "CD", lev: "EX", kan: "EX", price: "4", genre: "ROCK/POP", discogs: "r43433443"},
-        { artist: "Beatles", title: "Beatles Greatest Hits", label: "Beatles Music Label", size: "LP", lev: "EX", kan: "EX", price: "20", genre: "ROCK/POP", discogs: "r333443"}
-    ])
+    const [records, setRecords] = useState([])
+
+    const getRecords = () => {
+        fetch("http://localhost:3001/records")
+            .then(response => response.json())
+            .then(responseData => setRecords(responseData))
+    }
+
+    useEffect(() => {
+        getRecords()
+    }, [])
 
     const [columnDefinitions, setColumnDefinitions] = useState([
-        { field: "artist", headerName: "Artisti", filter: true, suppressMovable: true, width: 150},
-        { field: "title", headerName: "Levyn nimi", filter: true, suppressMovable: true},
-        { field: "label", headerName: "Levy-yhtiö", filter: true, suppressMovable: true},
-        { field: "size", headerName: "Koko", filter: true, suppressMovable: true, width: 150},
-        { field: "lev", headerName: "Rec", filter: true, suppressMovable: true, width: 150},
-        { field: "kan", headerName: "PS", filter: true, suppressMovable: true, width: 150},
-        { field: "price", headerName: "Hinta", filter: true, suppressMovable: true, width: 150},
-        { field: "genre", headerName: "Genre", filter: true, suppressMovable: true, width: 150},
-        { field: "discogs", headerName: "Discogs", filter: true, suppressMovable: true, width: 170},
+        { field: "artist", headerName: "Artisti", filter: true, suppressMovable: true, flex: 1 },
+        { field: "title", headerName: "Levyn nimi", filter: true, suppressMovable: true, flex: 1 },
+        { field: "label", headerName: "Levy-yhtiö", filter: true, suppressMovable: true, flex: 1 },
+        { field: "size", headerName: "Koko", filter: true, suppressMovable: true, flex: 1 },
+        { field: "lev", headerName: "Rec", filter: true, suppressMovable: true, flex: 1 },
+        { field: "kan", headerName: "PS", filter: true, suppressMovable: true, flex: 1 },
+        { field: "price", headerName: "Hinta", filter: true, suppressMovable: true, flex: 1 },
+        { field: "genre", headerName: "Genre", filter: true, suppressMovable: true, flex: 1 },
+        { field: "discogs", headerName: "Discogs", filter: true, suppressMovable: true, flex: 1 },
         {
-            cellRenderer: params => <Button size="small" color="success" onClick={() => addToChart(params.data)}>Lisää Koriin</Button>
+            cellRenderer: params => <Button size="small" color="success" onClick={() => addToChart(params.data)}>Lisää Koriin</Button>,
+            flex: 1
         },
         {
-            cellRenderer: params => <Button size="small" color="error" onClick={() => deleteRecord(params.data)}>Poista</Button>
+            cellRenderer: params => <Button size="small" color="error" onClick={() => deleteRecord(params.data)}>Poista</Button>,
+            flex: 1
         }
-    ])
+    ]);
+    
 
     const deleteRecord = (data) => {
-        console.log(`deleting data: ${data}`)
+        if(window.confirm("Oletko varma että haluat poistaa levyn?")) {
+            fetch(`http://localhost:3001/records/${data.id}`, {method: "DELETE"})
+            .then(response => {
+                if (response.ok) {
+                    getRecords()
+                } else {
+                    alert("Jotain meni vikaan.")
+                }
+            })
+        }
     }
 
     const addToChart = (data) => {
@@ -41,8 +60,14 @@ function Records() {
     return (
         <>
             <h1 style={{textAlign: "center"}}>Levykaupan levylista</h1>
-            <div className="ag-theme-material trainings" style={{ height: '2000px', width: "95%", margin: "auto" }}>
-                <AgGridReact rowData={records} columnDefs={columnDefinitions} />
+            <div className="ag-theme-material trainings" style={{ height: "1000px", width: "95%", margin: "auto" }}>
+                <AgGridReact
+                rowData={records}
+                columnDefs={columnDefinitions}
+                pagination={true}
+                paginationPageSize={20}
+                domLayout="auto"
+                />
             </div>
         </>
     )
